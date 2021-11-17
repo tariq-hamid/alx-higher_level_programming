c2771277591882c3ee7c010a1a0bde951f3e63fa
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-17 23:52:36 +0300

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if number % 10 > 5:
     print("and is greater than 5")
 elif number % 10 == 0:
     print("and is 0")
-else:
+elif number % 10 < 6 and number % 10 != 0:
     print("and is less than 6 and not 0")
