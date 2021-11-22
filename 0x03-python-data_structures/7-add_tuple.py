4fad7788aaa6fd26e084613d0bc93df23eb6bc33
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-22 19:06:08 +0300

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Function that adds 2 tuples.
+
+    Args:
+        tuple_a: first tuple
+        tuple_b: second tuple
+
+    Returns:
+        Nothing
+    """
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    if len(list_a) == 1:
+        list_a.append(0)
+    elif len(list_a) == 0:
+        list_a = [0, 0]
+    elif len(list_b) == 1:
+        list_b.append(0)
+    elif len(list_b) == 0:
+        list_b = [0, 0]
+    new_list = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
+    return tuple(new_list)
