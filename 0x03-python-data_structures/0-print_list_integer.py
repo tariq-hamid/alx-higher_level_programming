641af0501ec8519a90b8d7e92d5ab718d86c54cb
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-22 10:01:31 +0300

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Function to print every element of a list in new line
+
+    Args:
+        my_list: list to be printed
+
+    Returns:
+        Nothing
+    """
     for i in my_list:
         print("{}".format(i))
