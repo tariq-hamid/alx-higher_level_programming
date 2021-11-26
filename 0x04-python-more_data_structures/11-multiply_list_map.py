81e070c29201b5da7f38b7cc22a4de7c2eb59f8c
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-26 21:26:26 +0300

    task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     """Function that returns a list
-	with all values multiplied by a number
-	without using any loops.
+    with all values multiplied by a number
+    without using any loops.
 
     Args:
         my_list: list to be used
@@ -13,8 +13,3 @@ def multiply_list_map(my_list=[], number=0):
     """
     new_list = list(map(lambda i: i * number, my_list))
     return new_list
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
