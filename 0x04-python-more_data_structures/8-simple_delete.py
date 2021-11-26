4e5fa23d44e5b4f17ed305ccf3942e76a8fe89f0
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-26 20:19:21 +0300

    task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """Function that deletes a key in a dictionary.
+
+    Args:
+        a_dictionary: dictionary to be used
+        key: key to be deleted
+
+    Returns:
+        Edited dict
+    """
+    try:
+        if key:
+            a_dictionary.pop(key)
+    except:
+        pass
+    return a_dictionary
+
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
