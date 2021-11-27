009c365fcc03b9a787f7225da92d469b40fae9c3
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-27 07:23:09 +0300

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,13 +12,10 @@ def roman_to_int(roman_string):
         return 0
     roman_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                      'C': 100, 'D': 500, 'M': 1000}
-    i = 0
     result = 0
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_int_map:
-            result += roman_int_map[roman_string[i:i+2]]
-            i += 2
-        else:
-            result += roman_int_map[roman_string[i]]
-            i += 1
+    for letter in roman_string:
+        result += roman_int_map[letter]
+    for x in ["IV", "IX", "XL", "XC", "CM", "CD"]:
+        if x in roman_string:
+            result -= (2 * roman_int_map[x[0]])
     return result
