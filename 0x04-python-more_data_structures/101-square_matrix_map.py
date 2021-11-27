8a0c38dd797b8399912ded6bbae357473a5972a9
Tariq Hamid <tariq86hamid@gmail.com>
2021-11-27 08:44:13 +0300

    task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(
-        map(lambda el: list(map(lambda i: pow(i, 2), el)), matrix)
-    )
+    return list(map(lambda el: list(map(lambda i: pow(i, 2), el)), matrix))
