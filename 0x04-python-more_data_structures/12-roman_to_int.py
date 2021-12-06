c51532dd260bb305f62862b04fed2b1e2e8578fb
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-06 08:04:36 +0300

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     Returns:
         Integer if roman number is valid, 0 otherwise
     """
-    if not isinstance(roman_string, str) or not roman_string:
+    if (not isinstance(roman_string, str)) or (not roman_string):
         return 0
     roman_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                      'C': 100, 'D': 500, 'M': 1000}
