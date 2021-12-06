e2a4a472dbfb6a0a75afdddcd4020079f25fa9a9
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-06 20:23:57 +0300

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end='')
             i += 1
             np += 1
-        except:
+        except Exception:
             print()
             return np
     print()
