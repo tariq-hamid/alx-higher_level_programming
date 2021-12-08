b2ad8d3a86652380027d74648d261c0b1bf306db
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-08 19:05:48 +0300

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -12,7 +12,7 @@ def safe_print_division(a, b):
     """
     try:
         result = a / b
-    except Exception:
+    except ZeroDivisionError:
         return None
     finally:
         print("Inside result: {}".format(result))
