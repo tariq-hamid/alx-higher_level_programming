29613ae8c844113b3e01348ec03fdad29852c924
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-10 13:57:03 +0300

    task 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Function that raises a name exception with a message.
+
+    Args:
+        message: message to be used
+
+    Returns:
+        Nth
+    """
+    raise NameError(message)
