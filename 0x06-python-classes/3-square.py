4e940717344e3d7e57e1316ffa88bd640a4d2163
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-10 19:47:54 +0300

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,11 +14,11 @@ class Square:
         Returns:
             Nth
         """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Function that returns the area of a square
