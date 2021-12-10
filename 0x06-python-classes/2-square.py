4e62bdd710e9891084b19650f862210dd43ced4f
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-10 19:39:25 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class to define a square"""
+
+
+class Square:
+    """Class to define a square"""
+    def __init__(self, size=0):
+        """Instantiation function
+
+        Args:
+            size: size of the square
+
+        Returns:
+            Nth
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
