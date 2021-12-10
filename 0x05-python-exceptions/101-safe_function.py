6371b40175ec61c96493cabbaa21d5c6211dfaa2
Tariq Hamid <tariq86hamid@gmail.com>
2021-12-10 14:11:34 +0300

    task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    """Function that executes a function safely.
+
+    Args:
+        value: value to be printed
+
+    Returns:
+        Returns result of a function, None otherwise.
+    """
+    result = None
+    try:
+        result = fct(*args)
+    except:
+        print("Exception: {}".format(err), file=stderr)
+    return result
