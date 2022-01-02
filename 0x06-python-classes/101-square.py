a89d776b5ba04f48a3504a9ef04317bd8cd9123d
Tariq Hamid <tariq86hamid@gmail.com>
2022-01-02 16:07:49 +0300

    task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -120,5 +120,5 @@ class Square:
             for i in range(self.__position[1]):
                 print('')
             for i in range(self.__size):
-                print(" " * self.__position[0], end='')
+                # print(" " * self.__position[0], end='')
                 print("#" * self.__size)
