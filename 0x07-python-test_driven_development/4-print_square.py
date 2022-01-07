8bd81f49a82e553de81b38bb756c1894467b21fc
Tariq Hamid <tariq86hamid@gmail.com>
2022-01-07 11:22:21 +0300

    task 2

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Module containing a function that prints a square """
+
+
+def print_square(size):
+    """Function that prints a square with the character #
+
+    Args:
+        size: length of the square
+
+    Returns:
+        nth
+    """
