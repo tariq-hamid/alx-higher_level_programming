4c618e0d27fd77acf91881f5cd56cbc14814d013
Tariq Hamid <tariq86hamid@gmail.com>
2022-01-07 11:39:38 +0300

    task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -11,3 +11,13 @@ def print_square(size):
     Returns:
         nth
     """
+    if not (isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            if j == size - 1:
+                print("#")
+            else:
+                print("#", end='')
