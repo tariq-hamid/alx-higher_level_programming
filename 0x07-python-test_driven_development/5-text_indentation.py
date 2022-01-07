388f434ee2b8949a1beef850b4224b9fa4ebf7ae
Tariq Hamid <tariq86hamid@gmail.com>
2022-01-07 12:02:01 +0300

    task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module containing a function that prints a string """
+
+
+def text_indentation(text):
+    """Function that prints a text with 2
+    new lines after each of these characters: ., ? and :
+    
+    Args:
+        text: string to be printed
+    
+    Returns:
+        nth
+    """
+    if not (isinstance(text, str)):
+        raise TypeError("text must be a string")
+    strings_ = []
+    line = ""
+    for c in text:
+        line += c
+        if c in [".", "?", ":"]:
+            strings_.append(line.strip())
+            line = ""
+    for line_ in strings_:
+        print("{}\n".format(line_))
+    
