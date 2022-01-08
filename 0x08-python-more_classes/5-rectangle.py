d99734573c61bfd3a5910bd2322fc56a230ca1fc
Tariq Hamid <tariq86hamid@gmail.com>
2022-01-08 12:34:09 +0300

    update

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -74,5 +74,5 @@ class Rectangle():
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
-    def __del__(self):
+    def __del__():
         print("Bye rectangle...")
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -77,4 +77,5 @@ class Rectangle():
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
+        self.number_of_instances -= 1
         print("Bye rectangle...")
